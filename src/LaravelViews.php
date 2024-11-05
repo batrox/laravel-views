97fad22d58525b29c1b1f3619eb8312a3be7cc26
Batrox <batrox@users.noreply.github.com>
2024-11-05 14:25:37 +0100

    Update LaravelViews.php

--- src/LaravelViews.php
+++ src/LaravelViews.php
@@ -72,7 +72,6 @@ class LaravelViews
     public function css($options = '')
     {
         $assets = [
-            'livewire' => FrontendAssets::styles(),
             'tailwindcss' => '<link rel="stylesheet" href="' . asset('/vendor/tailwind.css') . '" />',
             'laravel-views' => '<link rel="stylesheet" href="' . asset('/vendor/laravel-views.css') . '" />'
         ];
@@ -83,8 +82,7 @@ class LaravelViews
     public function js($options = '')
     {
         $assets = [
-            'livewire' => FrontendAssets::scripts(),
-            'alpine' => '<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>',
+            'alpine' => '<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v3.x.x/dist/alpine.min.js" defer></script>',
             'laravel-views' => '<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.jsdelivr.net/npm/pikaday/pikaday.js"></script>
 <script src="' . asset('/vendor/laravel-views.js') . '" type="text/javascript" defer></script>'
