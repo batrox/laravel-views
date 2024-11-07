0182d08a8effcc7c4bde3db9c22908d249599946
Batrox <batrox@users.noreply.github.com>
2024-11-07 12:22:17 +0100

    Update icon.blade.php

--- resources/views/components/actions/icon.blade.php
+++ resources/views/components/actions/icon.blade.php
@@ -1,9 +1,13 @@
 @props(['actions', 'model'])
 
 @foreach ($actions as $action)
-  @if ($action->renderIf($model, $this))
-    <x-lv-tooltip :tooltip="$action->title">
-      <x-lv-icon-button :icon="$action->icon" size="sm" wire:click.prevent="executeAction('{{ $action->id }}', '{{ $model->getKey() }}')" />
-    </x-lv-tooltip>
-  @endif
+    @if ($action->renderIf($model, $this))
+        <x-lv-tooltip :tooltip="$action->title">
+            <x-lv-icon-button :icon="$action->icon" size="sm"
+                              wire:click.prevent="executeAction('{{ $action->id }}', '{{ $model->getKey() }}')"
+                              @if ($action->shouldBeConfirmed())
+                                  wire:confirm="{{ $action->getConfirmationMessage($model ? $this->getModelWhoFiredAction($model->getId()) : null) }}"
+                @endif/>
+        </x-lv-tooltip>
+    @endif
 @endforeach
